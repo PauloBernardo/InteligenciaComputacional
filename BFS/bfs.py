ccbb9c0052c9533cc974de89382eff4545771a26
pauloBenardo <paulobernardo06@gmail.com>
2021-08-30 23:29:27 -0300

    bfs

---
+++ BFS/bfs.py
@@ -0,0 +1,39 @@
+from collections import defaultdict
+
+
+class Graph:
+
+    def __init__(self):
+        self.graph = defaultdict(list)
+
+    def add_edge(self, u, v):
+        self.graph[u].append(v)
+
+    def BFS(self, s):
+
+        visited = [False] * (max(self.graph) + 1)
+        queue = [s]
+
+        visited[s] = True
+
+        while queue:
+            s = queue.pop(0)
+            print(s, end=" ")
+            for i in self.graph[s]:
+                if not visited[i]:
+                    queue.append(i)
+                    visited[i] = True
+
+
+if __name__ == "__main__":
+    g = Graph()
+    g.add_edge(0, 1)
+    g.add_edge(0, 2)
+    g.add_edge(1, 2)
+    g.add_edge(2, 0)
+    g.add_edge(2, 3)
+    g.add_edge(3, 3)
+
+    print("Following is Breadth First Traversal"
+          " (starting from vertex 2)")
+    g.BFS(3)
